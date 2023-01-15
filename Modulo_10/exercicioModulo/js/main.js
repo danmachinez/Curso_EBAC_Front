e6abaf87ddef28b3e08fcc30fa0eec8a5b41acf5
DanMach <machinezdanilo@gmail.com>
2023-01-15 15:01:22 -0300

    fix: alteracao no codigo do jQuery

--- Modulo_10/exercicioModulo/js/main.js
+++ Modulo_10/exercicioModulo/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-    $('#telefone').mask('(00) 00000-0000');
+    var $cpf = $('#cpf');
+    var $telefone = $('#telefone');
+    var $cep = $('#cep');
 
-    $('#cpf').mask('000.000.000-00');
-
-    $('#cep').mask('00000-000');
+    $cpf.mask('000.000.000-00');
+    $telefone.mask('(00) 00000-0000');
+    $cep.mask('00000-000'); 
 })
